759ec80dc5f5c7845903719531227ea6bb515f9e
rjmandloi <rjmandloi@isystango.com>
2022-05-06 11:50:42 +0530

    Product history fixing

--- JS/productHistory.js
+++ JS/productHistory.js
@@ -1,9 +1,15 @@
 import { db, set, ref, get, child, update, remove } from "./firebase.js";
+import { userDeatils } from "./fetchUserData.js";
+
 let userDATA = JSON.parse(localStorage.getItem("USERDATA"));
 const databaseRef = ref(db);
 
-userProductHistory();
+userProductHistory().then(()=>{
+   
+     
+});
 async function userProductHistory() {
+    
     let purchasedProductCounter = 1;
     let  soldProductCounter = 1;
     let purchasedProductTable = document.getElementById("purchasedProductTable");
@@ -19,6 +25,8 @@ async function userProductHistory() {
       <th scope="col">Product name</th>
       <th scope="col">Starting Price</th>
       <th scope="col">Final price</th>
+      <th scope="col">Buyer/Seller</th>
+
     </tr>
   </thead>
      `;
@@ -35,12 +43,13 @@ async function userProductHistory() {
                     // console.log(winners.key);//product key 
                     if (winners.val().SellerID == userDATA.id)//for sold products 
                     {
-                        console.log(winners.val().BuyerBidMoney);
+                        // console.log(winners.val().BuyerBidMoney);
                         get(child(databaseRef, `User/${winners.val().SellerID}/Details/ProductSold/${winners.key}`)).then((soldProduct) => {
                             if (typeof (soldProduct) !== 'undefined') {
                                 if (soldProduct.exists()) {
-                                    console.log(soldProduct.val());
-                                    
+                                    console.log(winners.val().BuyerID);
+                                    let buyerName = userDeatils.find(user => user.id === `${winners.val().BuyerID}`);
+                                    console.log(buyerName. FirstName);
                                     let tableContent = `
                                     <tr class="tableData">
                                     <th scope="row">${soldProductCounter}</th>
@@ -49,9 +58,11 @@ async function userProductHistory() {
                                     <td>${soldProduct.val().ProductName}</td>
                                     <td>${soldProduct.val().ProductPrice}</td>
                                     <td>${winners.val().BuyerBidMoney}</td>
+                                    <td>${buyerName. FirstName}</td>
                                     </tr>
 
                                     `;
+                                    
                                     soldProductTable.innerHTML +=tableContent;
                                     soldProductCounter++;
 
@@ -63,8 +74,9 @@ async function userProductHistory() {
                         get(child(databaseRef, `User/${winners.val().SellerID}/Details/ProductSold/${winners.key}`)).then((purchasedProduct) => {
                             if (typeof (purchasedProduct) !== 'undefined') {
                                 if (purchasedProduct.exists()) {
-                                    console.log(purchasedProduct.val());
-
+                                    let sellerName = userDeatils.find(user => user.id === `${winners.val().SellerID}`);
+                                    console.log(sellerName.FirstName);
+                                    // document.getElementById("seller").innerHTML="Seller";
                                     let tableContent = `
                                     <tr class="tableData">
                                     <th scope="row">${purchasedProductCounter}</th>
@@ -73,6 +85,8 @@ async function userProductHistory() {
                                     <td>${purchasedProduct.val().ProductName}</td>
                                     <td>${purchasedProduct.val().ProductPrice}</td>
                                     <td>${winners.val().BuyerBidMoney}</td>
+                                    <td>${sellerName.FirstName}</td>
+                                    
                                     </tr>
 
                                     `;
